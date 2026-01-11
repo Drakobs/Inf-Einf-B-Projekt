f6c099a1cdfa6aa81569a9bdbde4bbbec45c4d8c
Johannes Riedel <>
2026-01-11 14:55:31 +0100

    #2 Player Movement - basic movement. DropFlyForce() vorerst nur als test.

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -1,12 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
 
-    
-    private void Awake()
-    {
+    private PlayerInput _playerInput;
+    private InputAction _flyAction;
 
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+
+        //Get Input and Action
+        _playerInput = GetComponent<PlayerInput>();
+        _flyAction = _playerInput.actions.FindAction("Fly");
+    }
+
+    void FixedUpdate()
+    {
+        // 5. Check if the action exists and is being pressed
+        if (_flyAction != null && _flyAction.IsPressed())
+        {
+            WizardMovement.Instance.Fly();
+            Debug.Log("InputManager: Fly() gets called");
+        }
+
+        //better movement feeling -> yforce = 0, when not pressed anymore
+        if (_flyAction.IsPressed() != true)
+        {
+            WizardMovement.Instance.DropFlyForce();
+        }
     }
 }
